f86467d3d1c5a6b22c8f3f6bf8194380f1a21595
name <varunbrahmane30@gmail.com>
2021-07-06 17:06:00 +0530

    [varun] added: Welcome to emp wage program

---
+++ EmpWage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class EmpWage
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp Wage program.");
+        }
+    }
+}
